9ef9453039f7c58cc743c4531a2c3cd404075b13
furkangomleksiz <furkangomleksiz@gmail.com>
2025-04-11 03:29:41 +0300

    Point to Render

--- ModelPlaneClient/src/environments/environment.ts
+++ ModelPlaneClient/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5005/api' // Your backend API base URL for development
+    apiUrl: 'https://modelplaneapp.onrender.com/api/Planes' // Your backend API base URL for development
   };
   
